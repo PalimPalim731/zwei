4e3497b9b2026cf83012908785a9ae642de34f1c
rawniz <dariuszghoshal@gmail.com>
2017-02-24 14:25:13 +0000

    24/02/2017

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root "static_pages#home"
   match 'signup', to: 'authentication#signup', via: [:get, :post]
   match "login",  to: 'authentication#login',  via: [:get, :post]
+  match 'contact',to: 'static_pages#contact',  via: [:get, :post]
+  match "home",   to: 'static_pages#home',     via: [:get, :post]
 end
