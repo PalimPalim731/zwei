8e815e4a1bade463d0d8149c5911a342969fb2a2
rawniz <dariuszghoshal@gmail.com>
2017-02-27 16:58:26 +0000

    27/02/17

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,42 +1,37 @@
 class User < ActiveRecord::Base
-  validates :name, presence: { message: "Name name is required"}
-  validates :email, presence: { message: "Email name is required" }, 
-                    uniqueness: { case_sensitive: false, 
-                                  message: "This email is already registered" }
+  validates :name, presence: { message: "Name name is required" }
+  validates :email, presence: { message: "Email name is required" },
+                    uniqueness: { case_sensitive: false, message: "This email is already registered" }
   validates :password, presence: { message: "Name name is required" }
-  
+
   # TODO - we will add later a check to make sure an email can only register once
-  
+
   def self.signup(params)
-    puts "\n\n ^^^^ We will signup the user with: Name: #{params[:name]};
-          email: #{params[:email]} and password: #{params[:password]} \n\n"
-    user = User.new(name: params[:name], email: params[:email], password: params[:password])
+  	puts "\n\n ^^^^ We will signup the user with: Name: #{params[:name]}; email: #{params[:email]} and password: #{params[:password]} \n\n"
+  	user = User.new(name: params[:name], email: params[:email], password: params[:password])
     if user.save
       puts "\n\n ^^^^ User signed up successfully: #{user.inspect} \n\n"
     else
       puts "\n\n ^^^^ Errors while saving: #{user.errors.inspect} \n\n"
     end
   end
-  
+
   def self.authenticate(params)
-    puts "\n\n ^^^^ We will authenticate the user with the email: #{params[:email]}
-          and password: #{params[:password]} \n\n"
+  	puts "\n\n ^^^^ We will authenticate the user with the email: #{params[:email]} and password: #{params[:password]} \n\n"
+    
     if params[:email] and !params[:email].blank?
       user = self.find_by(email: params[:email])
-      
+
       # TODO - after we store the encrypted password, we will check that the stored encrypted password 
-      # is the same with the password submitted after we encrypt it
       
       if user and user.password == params[:password]
-        puts "\n\n ^^^^ We've found the user with the email #{params[:email]} 
-        and password #{params[:password]}. Log them in. \n\n"
+        puts "\n\n ^^^^ We've found the user with email #{params[:email]} and password #{params[:password]}. Log them in. \n\n"
         return user
       end
     end
     
     puts "\n\n ^^^^ We have not found the requested user, we'll just return a blank user \n\n"
-    # just return an empty user
     self.new
   end
-      
+
 end
